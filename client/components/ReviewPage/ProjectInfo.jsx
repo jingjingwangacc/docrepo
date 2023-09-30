334bb7e6c143c639bd2d551ce01bac83001d4dd1
jingjing <jingjingwangacc@gmail.com>
2023-09-29 21:55:52 -0700

    fix css in project info.

--- client/components/ReviewPage/ProjectInfo.jsx
+++ client/components/ReviewPage/ProjectInfo.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const ProjectInfo = props => {
   return (
     <div className='reviewProjectInfo'>
-      <label className='reviewLabel1'>Project Info</label>
+      <h2>Project Info</h2>
       <div>
-        <label reviewLabel2>Project Name: </label>
+        <label>Project Name: </label>
         <span>{props.projectName}</span>
       </div>
       <div>
